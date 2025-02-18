d13590b831fa64dd86e71defb480e90a50d3433a
Tatsumaki0619 <tatsumakiK6@gmail.com>
2025-02-18 06:28:28 -0800

    finish ProChall15

--- Lab2Pt2ProCHAll15.java
+++ Lab2Pt2ProCHAll15.java
@@ -1,11 +1,36 @@
 
 /**
- * Write a description of class Lab2Pt2ProCHAll15 here.
+ * This is the last challenge for Lab2pt2. the out put is given by using dialog with paid for shares,
+ * commission and total cost.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Qianyi Li
+ * @version 1.0
+ * @since 2/15/2025
  */
+import javax.swing.JOptionPane;
 public class Lab2Pt2ProCHAll15
 {
-    
+    public static void main(String []args){
+        final double COMMISION_FEE;
+        COMMISION_FEE = 0.02;
+        double pricePerShare, shareFee, commisionFee, totalFee;
+        int number;
+        String inputValue;
+        
+        inputValue = JOptionPane.showInputDialog("What is the" + " price per share?");
+        pricePerShare = Double.parseDouble(inputValue);
+        
+        inputValue = JOptionPane.showInputDialog("What is the" + " amount of share?");
+        number = Integer.parseInt(inputValue); //the first "Int" written in full "Integer"
+        
+        shareFee = number * pricePerShare;
+        commisionFee = shareFee * COMMISION_FEE;
+        totalFee = shareFee + commisionFee;
+        shareFee = Math.round(shareFee * 100.0)/100.0;
+        commisionFee = Math.round(commisionFee * 100.0)/100.0;
+        totalFee = Math.round(totalFee * 100.0)/100.0;
+        
+        JOptionPane.showMessageDialog(null, "Total cost of shares are: $" + shareFee + '\n' + "Commission cost is: $" + commisionFee + '\n' + "Total Cost is: $" + totalFee);
+        System.exit(0);
+    }
 }
